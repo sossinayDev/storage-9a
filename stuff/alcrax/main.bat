4fb262de949c04c1db1200db74c7bb134deb2722
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-10-16 12:55:28 +0200

    Added fun stuff

--- stuff/alcrax/main.bat
+++ stuff/alcrax/main.bat
@@ -1,5 +1,13 @@
-msg %username% I hacked you.
-msg %username% Wanna die?
-msg %username% I got you...
+@echo off
+start https://sossinaydev.github.io/other_pages/sketchy/sketchy.html
+msg %username% You won a gift!
 
-shutdown /s /t 120
+echo start cmd /k "color 2 & cd C:/ & tree & msg %username% Access granted!" > fun2.bat
+echo timeout /t 3 /nobreak  >> fun2.bat
+echo start cmd /k "color 16 & cd C:/ & tree  & msg %username% Files transferred!" >> fun2.bat
+echo timeout /t 3 /nobreak  >> fun2.bat
+echo start cmd /k "color 4 & cd C:/ & tree  & msg %username% Files removed!" >> fun2.bat
+echo del "fun2.bat" >> fun2.bat
+
+
+del "main.bat"
