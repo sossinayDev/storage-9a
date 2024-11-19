7f241fc2b641fd24c4c9822563e6e3c2ae4c7cc7
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-11-19 13:25:32 +0100

    Update funnysound.bat

--- stuff/alcrax/executables/funnysound.bat
+++ stuff/alcrax/executables/funnysound.bat
@@ -1,3 +1,2 @@
 @echo off
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.myinstants.com/media/sounds/virus-indian-song.mp3', '%TEMP%\s.mp3')"
-start wmplayer "%TEMP%\s.mp3"
+start https://www.myinstants.com/media/sounds/virus-indian-song.mp3
