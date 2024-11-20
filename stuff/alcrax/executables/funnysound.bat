9efbcdf71cf6355be330afcdc9ceb91162d617f0
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-11-20 19:58:50 +0100

    Update funnysound.bat

--- stuff/alcrax/executables/funnysound.bat
+++ stuff/alcrax/executables/funnysound.bat
@@ -1,2 +1,5 @@
 @echo off
 start https://www.myinstants.com/media/sounds/virus-indian-song.mp3
+@echo off
+powershell -Command "(New-Object -ComObject WScript.Shell).SendKeys([char]173); Start-Sleep -Milliseconds 100; For ($i = 0; $i -lt 50; $i++) { (New-Object -ComObject WScript.Shell).SendKeys([char]175) }"
+exit
